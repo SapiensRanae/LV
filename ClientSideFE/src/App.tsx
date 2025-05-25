17ef02e3560026dd566db2dea57aa9cb3757471f
ranae <uraucovichura@gmail.com>
2025-05-25 18:47:10 +0200

    sdadsad

---
+++ ClientSideFE/src/App.tsx
@@ -0,0 +1,35 @@
+// src/App.tsx
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Games from './pages/Games';
+import Guides from './pages/Guides';
+import About from './pages/About';
+import Terms from './pages/Terms';
+import CookiePolicy from './pages/CookiePolicy';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import './App.css';
+
+const App: React.FC = () => {
+    return (
+        <>
+            <Navbar />
+            <div className="content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/games" element={<Games />} />
+                    <Route path="/guides" element={<Guides />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/terms" element={<Terms />} />
+                    <Route path="/cookie-policy" element={<CookiePolicy />} />
+                    <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+                </Routes>
+            </div>
+            <Footer />
+        </>
+    );
+};
+
+export default App;
--- ClientSideFE/src/index.jsx
+++ ClientSideFE/src/index.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Base from './Base';
-import Test from './Test';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Base />} />
-            <Route path="/test" element={<Test />} />
-        </Routes>
-    </BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
